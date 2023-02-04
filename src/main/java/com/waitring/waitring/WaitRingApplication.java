a792355b618e18f40a9e1d4791fd16d0431798f0
soohyun <soohyun6879@naver.com>
2023-02-04 15:19:24 +0900

    Initial commit

---
+++ src/main/java/com/waitring/waitring/WaitRingApplication.java
@@ -0,0 +1,13 @@
+package com.waitring.waitring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitRingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaitRingApplication.class, args);
+    }
+
+}
